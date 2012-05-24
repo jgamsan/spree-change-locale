08a5a3332bce701ec5486ebe2a1e62037eab3693
Jose Miguel Gamazo Sanchez <jose@jose-System-Product-Name.(none)>
2012-05-24 17:23:08 +0200

    Adapted to spree 1.1.1

--- lib/spree_change_locale/i18n_extension.rb
+++ lib/spree_change_locale/i18n_extension.rb
@@ -1,6 +1,6 @@
 module I18nExtension
   def enabled_locales
-    @@enabled_locales ||= (Spree::Config.get(:enabled_locales) || get_locales || I18n.available_locales || [I18n.default_locale]).collect(&:to_sym)
+    @@enabled_locales ||= get_locales || I18n.available_locales || [I18n.default_locale]).collect(&:to_sym)
   end
   def locale_enabled?(locale)
     enabled_locales.include?(locale.to_sym)
