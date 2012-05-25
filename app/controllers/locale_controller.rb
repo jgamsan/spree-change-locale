c247f96378ff91c978e1171802b316c7566e6002
Jose Miguel Gamazo Sanchez <jose@jose-System-Product-Name.(none)>
2012-05-25 18:38:09 +0200

    Mas cambios en redireccion

--- app/controllers/locale_controller.rb
+++ app/controllers/locale_controller.rb
@@ -2,6 +2,6 @@ class LocaleController < Spree::BaseController
   
   def set
     session[:locale] = I18n.locale = params[:locale].to_sym if params[:locale] && I18n.available_locales.include?(params[:locale].to_sym)
-    redirect_back_or_default(params[:return_path] ? "/#{params[:return_path]}" : products_path)
+    redirect_to : products_path)
   end
 end
