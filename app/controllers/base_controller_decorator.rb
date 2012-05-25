8add09db532c9aab2688aed74c91b345e5949150
Jose Miguel Gamazo Sanchez <jose@jose-System-Product-Name.(none)>
2012-05-25 21:45:23 +0200

    Mas cambios en redireccion

--- app/controllers/base_controller_decorator.rb
+++ app/controllers/base_controller_decorator.rb
@@ -1,9 +1,11 @@
 Spree::BaseController.class_eval do
-  layout 'initial'
-  before_filter :set_locale, :if => proc { !session[:locale] }
+  
+  before_filter :set_settings, :if => proc { !session[:locale] }
 
-  protected
-  def set_locale
-    session[:locale] = I18n.locale = (request.preferred_language_from(I18n.enabled_locales) || request.compatible_language_from(I18n.enabled_locales) || I18n.default_locale).to_sym
-  end
+  def set_settings
+    unless session[:locale]
+      redirect_to locale_path and return
+    end
+  end 
+  
 end
--- app/controllers/locale_controller.rb
+++ app/controllers/locale_controller.rb
@@ -1,7 +1,12 @@
 class LocaleController < Spree::BaseController
-  
+  layout 'initial'
   def set
     session[:locale] = I18n.locale = params[:locale].to_sym if params[:locale] && I18n.available_locales.include?(params[:locale].to_sym)
     redirect_to products_path
+  end
+  
+  protected
+  def set_locale
+    session[:locale] = I18n.locale = (request.preferred_language_from(I18n.enabled_locales) || request.compatible_language_from(I18n.enabled_locales) || I18n.default_locale).to_sym
   end
 end
