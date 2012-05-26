acab37950c8bd66c1f78063c473679d100af552f
Jose Miguel Gamazo Sanchez <jose@jose-System-Product-Name.(none)>
2012-05-26 10:32:25 +0200

    Modificada ruta

--- app/controllers/base_controller_decorator.rb
+++ app/controllers/base_controller_decorator.rb
@@ -3,7 +3,7 @@ Spree::BaseController.class_eval do
   before_filter :set_settings, :if => proc { !session[:locale] }
 
   def set_settings
-    unless session[:locale].empty? || session[:locale].nil?
+    if session[:locale].nil?
       redirect_to change_locale_path and return
     end
   end 
