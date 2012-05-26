6d811927dddb4683368943e7dcbdc4323aa44ad2
Jose Miguel Gamazo Sanchez <jose@jose-System-Product-Name.(none)>
2012-05-26 10:47:17 +0200

    Modificada ruta

--- app/controllers/base_controller_decorator.rb
+++ app/controllers/base_controller_decorator.rb
@@ -4,7 +4,7 @@ Spree::BaseController.class_eval do
 
   def set_settings
     if session[:locale].nil?
-      redirect_to :controller => "locale", :action => "set"
+      render :controller => "locale", :action => "set"
     end
   end 
   
